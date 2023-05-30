99432a63ca4d328681777d4f1c06c4906cd5f63f
svenfilify <sven@filify.app>
2023-05-30 11:48:15 +0300

    removed redirect

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,5 @@ import { redirect } from "next/navigation";
 import HomePage from "./HomePage";
   
 export default function Page() {
-    // return <HomePage />
-    return redirect('/projects');
+    return <HomePage />;
 }
